496b8d449e9d69f3fdf852fdd1406077628af4e1
olika <myshaka@narod.ru>
2021-11-26 20:01:56 +0300

    commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO `roles` (`role`) VALUES ('ROLE_USER');
+INSERT INTO `roles` (`role`) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `users` (`name`, `lastname`, `email`, `age`) VALUES ('user',
+                                                                 'sam', 'user@mail.ru', 19);
+INSERT INTO `users` (`name`,`lastname`, `email`, `age`) VALUES ('useradmin','lil', 'useradmin@mail.ru', 28);
+INSERT INTO `users` (`name`, `lastname`, `email`, `age`) VALUES ('admin','zz', 'admin@mail.ru', 36);
+
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (1, 1);
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (3, 2);
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (2, 1);
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (2, 2);
+
+UPDATE `users`
+SET password='$2a$12$ws5QO3VZ5CF.BuWxm5QWYOeWNK25Ipa8pIS2coDfpStR5QZmadPI6'
+where name='user';
+
+UPDATE `users`
+SET password='$2a$12$ws5QO3VZ5CF.BuWxm5QWYOeWNK25Ipa8pIS2coDfpStR5QZmadPI6'
+where name='useradmin';
+
+UPDATE `users`
+SET password='$2a$12$ws5QO3VZ5CF.BuWxm5QWYOeWNK25Ipa8pIS2coDfpStR5QZmadPI6'
+where name='admin';
+
+#password = 1
